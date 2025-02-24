9e37129f6f1564b3597768b3814ed3057ddd8f0e
BhuvanThota <teacherb3337@gmail.com>
2025-02-24 17:21:24 +0530

    Day-01- Updated

--- Basics/Demo.java
+++ Basics/Demo.java
@@ -1,27 +1,36 @@
 package Basics;
 
-class Calc{
+class Student{
 
+    int rollno;
+    String name;
+    int marks;
 
-    public int add(int a, int b) {
-        return a+b;
+    Student (int rollno, String name, int marks){
+        this.rollno = rollno;
+        this.name = name;
+        this.marks = marks;
     }
 
-    public int add(int a, int b, int c) {
-        return a+b+c;
-    }
 }
 
 public class Demo {
     public static void main(String[] args){
-        int a = 10;
-        int b = 5;
-        int c = 6;
 
-        Calc cal = new Calc();
-        int res = cal.add(a,b,c);
+        Student s1 = new Student(1, "A",69);
+        Student s2 = new Student(2, "B",96);
+        Student s3 = new Student(3, "C",78);
 
-        System.out.println(res);
+        Student students[] = new Student[3];
+        students[0] = s1;
+        students[1] = s2;
+        students[2] = s3;
+
+        // System.out.println(students[1].name +": "+ students[1].marks);
+
+
 
     }
 }
+
+
